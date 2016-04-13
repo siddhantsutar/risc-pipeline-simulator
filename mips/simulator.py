355770785fa7fc1de7b3087619c0f0b776dd6188
siddhantsutar <siddhantsutar@users.noreply.github.com>
2016-04-13 15:00:27 -0500

    Updated simulator with pipelined datapath

--- mips/simulator.py
+++ mips/simulator.py
@@ -1,104 +1,97 @@
 import re
 import csv
 
+machine_code_file = open("test.mips", "r")
+
 pc = 0
 instruction_memory = []
-data_memory = []
-register_file = ["00000000", "00000001", "00000010", "00000011", "00000100", "00000000", "00000000", "00000000"]
-machine_code_file = open("test.mips", "r")
+
+# Registers $6 and $7 will be reserved as base addresses for referencing data memory
+register_file = ["0000000000000000", "0000000000000001", "0000000000000010", "0000000000000011", "0000000000000100", None, None, None]
+
+"""
+In our implementation, each word is 16 bits (2 bytes) long. MIPS is Big Endian, so we will store the most and least significant bytes
+at indices i and i+1 respectively. Typically, our addresses would be range from 0 to 65535 (2**16 - 1), but we do not need to include
+these many since we are supposed to output the data memory.
+"""
+data_memory = [] * 64 
 
 for each in machine_code_file:
     i = re.sub('\s+', '', each)
     instruction_memory.append(i[0:8])
     instruction_memory.append(i[8:16])
 
-for i in range(8):
-    data_memory.append([None] * 4)
-
 """
-PIPELINE REGISTERS
-IF/ID: stores 16-bit instruction
-ID/EX: stores EX, MEM, WB control signals + Read data 1, Read data 2, Rt, Rd, SEImm
-EX/MEM: stores MEM, WB control signals + Zero, ALU Result, Read data 2, Write register
-MEM/WB: stores WB control signals + ALU Result, Read data
+PIPELINE REGISTERS (MEM signals also include Jump and Branch_NE)
+IF/ID: stores 16-bit instruction + PC+2
+ID/EX: stores EX, MEM, WB control signals + JumpAddr, PC+2, Read data 1, Read data 2, Rt, Rd, SEImm
+EX/MEM: stores MEM, WB control signals + JumpAddr, BranchAdder, Zero, ALU Result, Read data 2, Write register
+MEM/WB: stores WB control signals + ALU Result, Read data, Write register
 """
 
-buffer = {'IF/ID': {'Instruction': None}, \
-           'ID/EX': {'RegDst': None, 'ALUSrc': None, 'ALUOp1': None, 'ALUOp0': None, 'Branch': None, 'MemRead': None, 'MemWrite': None, 'MemToReg': None, 'RegWrite': None, 'Read data 1': None, 'Read data 2': None, 'Rt': None, 'Rd': None, 'SEImm': None}, \
-           'EX/MEM': {'Branch': None, 'MemRead': None, 'MemWrite': None, 'MemToReg': None, 'RegWrite': None, 'Zero': None, 'ALU Result': None, 'Read data 2': None}, \
-           'MEM/WB': {'MemToReg': None, 'RegWrite': None, 'ALU Result': None, 'Read data': None}}
+buffer = {'IF/ID': {'PC+2': None, 'Instruction': None}, \
+           'ID/EX': {'RegDst': None, 'ALUSrc': None, 'ALUOp1': None, 'ALUOp0': None, 'Jump': None, 'Branch': None, 'Branch_NE': None, 'MemRead': None, 'MemWrite': None, 'MemToReg': None, 'RegWrite': None,  'JumpAddr': None, 'PC+2': None, 'Read data 1': None, 'Read data 2': None, 'Rt': None, 'Rd': None, 'SEImm': None}, \
+           'EX/MEM': {'Jump': None, 'Branch': None, 'Branch_NE': None, 'MemRead': None, 'MemWrite': None, 'MemToReg': None, 'RegWrite': None, 'JumpAddr': None, 'BranchAdder': None, 'Zero': None, 'ALU Result': None, 'Read data 2': None, 'Write register': None}, \
+           'MEM/WB': {'MemToReg': None, 'RegWrite': None, 'ALU Result': None, 'Read data': None, 'Write register': None}}
+
+def init_signals(RegDst, ALUSrc, ALUOp1, ALUOp0, Jump, Branch, Branch_NE, MemRead, MemWrite, MemToReg, RegWrite):
+    buffer['ID/EX']['RegDst'] = RegDst
+    buffer['ID/EX']['ALUSrc'] = ALUSrc
+    buffer['ID/EX']['ALUOp1'] = ALUOp1
+    buffer['ID/EX']['ALUOp0'] = ALUOp0
+    buffer['ID/EX']['Jump'] = Jump
+    buffer['ID/EX']['Branch'] = Branch
+    buffer['ID/EX']['Branch_NE'] = Branch_NE
+    buffer['ID/EX']['MemRead'] = MemRead
+    buffer['ID/EX']['MemWrite'] = MemWrite
+    buffer['ID/EX']['MemToReg'] = MemToReg
+    buffer['ID/EX']['RegWrite'] = RegWrite
 
 def control_unit(opcode):
     if opcode == "0000": #R-format
-        buffer['ID/EX']['RegDst'] = 1
-        buffer['ID/EX']['ALUSrc'] = 0
-        buffer['ID/EX']['ALUOp1'] = 1
-        buffer['ID/EX']['ALUOp0'] = 0
-        buffer['ID/EX']['Branch'] = 0
-        buffer['ID/EX']['MemRead'] = 0
-        buffer['ID/EX']['MemWrite'] = 0
-        buffer['ID/EX']['MemToReg'] = 0
-        buffer['ID/EX']['RegWrite'] = 1
+        init_signals(1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1)
     elif opcode == "0111" : #lw
-        buffer['ID/EX']['RegDst'] = 0
-        buffer['ID/EX']['ALUSrc'] = 1
-        buffer['ID/EX']['ALUOp1'] = 0
-        buffer['ID/EX']['ALUOp0'] = 0
-        buffer['ID/EX']['Branch'] = 0
-        buffer['ID/EX']['MemRead'] = 1
-        buffer['ID/EX']['MemWrite'] = 0
-        buffer['ID/EX']['MemToReg'] = 1
-        buffer['ID/EX']['RegWrite'] = 1
+        init_signals(0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1)
     elif opcode == "1010" : #sw
-        buffer['ID/EX']['RegDst'] = "X"
-        buffer['ID/EX']['ALUSrc'] = 1
-        buffer['ID/EX']['ALUOp1'] = 0
-        buffer['ID/EX']['ALUOp0'] = 0
-        buffer['ID/EX']['Branch'] = 0
-        buffer['ID/EX']['MemRead'] = 0
-        buffer['ID/EX']['MemWrite'] = 1
-        buffer['ID/EX']['MemToReg'] = "X"
-        buffer['ID/EX']['RegWrite'] = 0
+        init_signals("X", 1, 0, 0, 0, 0, 0, 0, 1, "X", 0)
     elif opcode == "0011" : #beq
-        buffer['ID/EX']['RegDst'] = "X"
-        buffer['ID/EX']['ALUSrc'] = 0
-        buffer['ID/EX']['ALUOp1'] = 0
-        buffer['ID/EX']['ALUOp0'] = 1
-        buffer['ID/EX']['Branch'] = 1
-        buffer['ID/EX']['MemRead'] = 0
-        buffer['ID/EX']['MemWrite'] = 0
-        buffer['ID/EX']['MemToReg'] = "X"
-        buffer['ID/EX']['RegWrite'] = 0
+        init_signals("X", 0, 0, 1, 0, 1, 0, 0, 0, "X", 0)
 
-def ALU_control(b):
-    a = buffer['ID/EX']['Read data 1'] # Read data 1
+def ALU_control():
     if buffer['ID/EX']['ALUOp1'] == 1 and buffer['ID/EX']['ALUOp0'] == 0:
         if buffer['ID/EX']['SEImm'][13:16] == "000":
-            buffer['EX/MEM']['ALU Result'] = format(int(a, 2) + int(b, 2), '08b')
+            return "operation"
             #find out the operation using buffer['ID/EX']['ALUOp1'], buffer['ID/EX']['ALUOp0'] and buffer['ID/EX']['SEImm'][13:16]
             #do relevant operation on a and b
             #store in buffer['EX/MEM']['ALU Result'], buffer['EX/MEM']['Zero']
+
+def ALU(b):
+    a = buffer['ID/EX']['Read data 1'] # Read data 1
+    if ALU_control() == "operation":
+        buffer['EX/MEM']['ALU Result'] = format(int(a, 2) + int(b, 2), '016b')
 
 def wb_stage():
     if buffer['MEM/WB']['RegWrite'] == 1:
         if buffer['MEM/WB']['MemToReg'] == 0:
             register_file[int(buffer['MEM/WB']['Write register'], 2)] = buffer['MEM/WB']['ALU Result']
-        else:
+        elif buffer['MEM/WB']['MemToReg'] == 1:
             register_file[int(buffer['MEM/WB']['Write register'], 2)] = buffer['MEM/WB']['Read data']
 
     clear_buffer("MEM/WB")
                         
         
 def mem_stage():
+    global pc
     buffer['MEM/WB']['MemToReg'] = buffer['EX/MEM']['MemToReg']
     buffer['MEM/WB']['RegWrite'] = buffer['EX/MEM']['RegWrite']
     buffer['MEM/WB']['Write register'] = buffer['EX/MEM']['Write register']
+
+    if buffer['EX/MEM']['Jump'] == 1:
+        pc = buffer['MEM/WB']['JumpAddr']
         
     # TO-DO
-    if buffer['EX/MEM']['Branch'] == 1:
-        if buffer['EX/MEM']['Zero'] == 1:                        
-            #PC calculation
-            return;
+    if buffer['EX/MEM']['Branch'] == 1 and buffer['EX/MEM']['Zero'] == 1:
+        pc = buffer['MEM/WB']['BranchAdder']
 
     if buffer['EX/MEM']['MemRead'] == 1:
         buffer['MEM/WB']['Read data'] = data_memory[int(buffer['EX/MEM']['ALU Result'], 2)]
@@ -117,21 +110,28 @@ def ex_stage():
     buffer['EX/MEM']['MemWrite'] = buffer['ID/EX']['MemWrite']
     buffer['EX/MEM']['MemToReg'] = buffer['ID/EX']['MemToReg']
     buffer['EX/MEM']['RegWrite'] = buffer['ID/EX']['RegWrite']
+
+    buffer['EX/MEM']['JumpAddr'] = buffer['ID/EX']['JumpAddr']
     buffer['EX/MEM']['Read data 2'] = buffer['ID/EX']['Read data 2']
 
-    if buffer['ID/EX']['RegDst'] == 1:
-        buffer['EX/MEM']['Write register'] = buffer['ID/EX']['Rd']
-    else:
+    #Check for overflow
+    buffer['EX/MEM']['BranchAdder'] = buffer['ID/EX']['PC+2'] + (int(buffer['ID/EX']['SEImm'], 2))
+
+    if buffer['ID/EX']['RegDst'] == 0:
         buffer['EX/MEM']['Write register'] = buffer['ID/EX']['Rt']
+    elif buffer['ID/EX']['RegDst'] == 1:
+        buffer['EX/MEM']['Write register'] = buffer['ID/EX']['Rd']
 
     if buffer['ID/EX']['ALUSrc'] == 0: #R-format instruction
-        ALU_control(buffer['ID/EX']['Read data 2'])
-    else: 
-        ALU_control(buffer['ID/EX']['SEImm'])
+        ALU(buffer['ID/EX']['Read data 2'])
+    elif buffer['ID/EX']['ALUSrc'] == 1: 
+        ALU(buffer['ID/EX']['SEImm'])
 
     clear_buffer("ID/EX")
                         
 def id_stage():
+    buffer['ID/EX']['JumpAddr'] = int(buffer['IF/ID']['Instruction'][4:16], 2) * 2
+    buffer['ID/EX']['PC+2'] = buffer['IF/ID']['PC+2']
     buffer['ID/EX']['Read data 1'] = register_file[int(buffer['IF/ID']['Instruction'][4:7], 2)]
     buffer['ID/EX']['Read data 2'] = register_file[int(buffer['IF/ID']['Instruction'][7:10], 2)]
     buffer['ID/EX']['Rt'] = buffer['IF/ID']['Instruction'][7:10]
@@ -142,8 +142,9 @@ def id_stage():
         
 def if_stage():
     global pc
+    buffer['IF/ID']['PC+2'] = pc + 2
     buffer['IF/ID']['Instruction'] = instruction_memory[pc] + instruction_memory[pc+1]
-    pc += 2
+    pc += 2 #At the end of the clock cycle, write to the program counter register
 
 def clear_buffer(b):
     keys = buffer[b].keys()
